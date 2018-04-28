d0089d5c1efd917771a2ac36cefa7eb16a46d1e4
Caio Sanches <ccampato@gmail.com>
2018-04-28 06:23:10 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+require 'pry'
+class School
+  attr_accessor :roster
+
+  def initialize(name)
+    @name = name
+    @roster = Hash.new { |hash, key| hash[key] = [] }
+  end
+
+  def add_student(student, grade)
+    @student = student
+    @student_array = @roster[grade] << student
+  end
+
+  def grade(grade)
+    @grade = grade
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |a, b|
+      b.sort
+    end
+  end
+end
